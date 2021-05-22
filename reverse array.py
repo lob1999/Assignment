1bf6fe7673f0fc06aab3e4810dcacfb717db6150
lob1999 <79892097+lob1999@users.noreply.github.com>
2021-05-22 11:41:01 +0530

    Add files via upload

---
+++ reverse array.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+T=int(input())
+ns=[]
+arr1=[]
+
+for i in range(T):
+    n=input().split(" ")
+    ns.append(n)
+    arr1.append(input().split(" "))
+    
+for i in range(T):
+    n=ns[i]
+    arr=arr1[i]
+    for i in range(len(arr)):
+        arr[i]=int(arr[i])
+        
+    arr.reverse()
+    print(arr)
